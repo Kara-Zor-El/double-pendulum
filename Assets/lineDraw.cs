fb9e0d1577bac78394688b93e844c19b8761d5a2
Kara-Zor-El <69772087+Kara-Zor-El@users.noreply.github.com>
2021-09-09 19:06:32 -0400

    initial commit

---
+++ Assets/lineDraw.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lineDraw : MonoBehaviour {
+    // Start is called before the first frame update
+    public LineRenderer lineRenderer;
+    public GameObject pivot;
+    public GameObject point0;
+    public float dist;
+    public GameObject tree;
+    GameObject newObj;
+
+    void Start() {
+        lineRenderer = GetComponent<LineRenderer>();
+        point0.transform.position = new Vector3(Random.Range(pivot.transform.position.x-2f, pivot.transform.position.x+2f), Random.Range(pivot.transform.position.y, pivot.transform.position.y+2f), 0);
+        
+    }
+
+    void Update(){
+        lineRenderer.SetPosition(0, pivot.gameObject.transform.position);
+        lineRenderer.SetPosition(1, point0.gameObject.transform.position);
+        point0.GetComponent<DistanceJoint2D>().distance = dist;
+        float pointDist = Vector2.Distance(point0.transform.position, pivot.transform.position);
+        if(Input.GetMouseButtonDown(1)){
+            newObj = Instantiate(tree);
+            // newObj = Instantiate (tree, tree.transform.position, Quaternion.identity);
+
+        }
+        // if(Input.GetKeyDown("r")){
+        //     Destroy(newObj);
+        // }
+        // Debug.Log(point0.name + " distance is " + pointDist);
+        // point0.GetComponent<HingeJoint2D>().connectedBody = point0.GetComponent<Rigidbody2D>();
+    }
+}
---
+++ Assets/soonStart.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class soonStart : MonoBehaviour {
+    public TrailRenderer tr;
+    // Start is called before the first frame update
+    void Start(){
+        // Color re = new Color(1f, 0f, 0f, 1f);
+        // Color or = new Color(1f, 0.6470588f, 0f, 1f);
+        // Color y = new Color(1f, 1f, 0f, 1f);
+        // Color g = new Color(0f, 1f, 0f, 1f);
+        // Color b = new Color(0f, 1f, 1f, 1f);
+        // Color p = new Color(1f, 0f, 1f, 1f);
+        // Color pu = new Color(0.7630649f, 0.1822268f, 0.990566f, 1f);
+        
+        tr.enabled = false;
+        // float alpha = 1.0f;
+        // Gradient gradient = new Gradient();
+        // gradient.SetKeys(
+        //     new GradientColorKey[] { new GradientColorKey(Color.red, 0.0f), new GradientColorKey(Color.yellow, 1.0f) },
+        //     new GradientAlphaKey[] { new GradientAlphaKey(alpha, 0.0f), new GradientAlphaKey(alpha, 1.0f) }
+        // );
+    }
+
+    // Update is called once per frame
+    void Update() {
+        StartCoroutine(test());
+        if(Input.GetMouseButtonDown(0)){
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+    IEnumerator test(){
+        yield return new WaitForSeconds(0.1f);
+        tr.enabled = true;
+    }
+}
